a78adfc5d295009294240a7193f8f2ba6f53bac2
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-04-22 01:15:41 +0900

    게시글 수정기능추가

--- router/post.js
+++ router/post.js
@@ -50,13 +50,21 @@ router.post('/create',function(req,res,next){
             for(i=0;i<files.length;i++){
                 insertQuery+="insert into pjh1352.postImage(post_id,filename) values("+result.insertId+",'"+files[i]+"');";
             }
-            connection.query(insertQuery,function(err,rows,fields){
-                if(err)throw err;
+            if(insertQuery==""){
                 connection.query(selectQuery,selectParams,function(err,rows,fields){
                     if(err)throw err;
                     res.status(200).json(new Object(rows[0]));
                 });
-            });
+            }else{
+                connection.query(insertQuery,function(err,rows,fields){
+                    if(err)throw err;
+                    connection.query(selectQuery,selectParams,function(err,rows,fields){
+                        if(err)throw err;
+                        res.status(200).json(new Object(rows[0]));
+                    });
+                });
+            }
+            
            
         });
         
@@ -64,11 +72,127 @@ router.post('/create',function(req,res,next){
     
 });
 
+//post update
+router.put('/update',function(req,res,next){
+    console.log("post create");
+    var post_id;
+    var content;
+    var deleteList;
+    var files=new Array();
+    var form=new multiparty.Form();
+    form.parse(req);
+    form.on('field',function(name,value){
+        console.log('field : '+name+"  value : "+value);
+        if(name=="postID")post_id=value;
+        if(name=="content")content=value.substring(1,value.length-1);
+        if(name=="userEmail")userEmail=value.substring(1,value.length-1);
+        if(name=="time")time=value;
+        if(name=="deleteList"){
+            console.log(JSON.parse(value)[0]);
+            deleteList=JSON.parse(value);
+        }
+    });
+    form.on('part',function(part){
+        files.push(part.name);
+        mkdirp(__dirname+'/../save/post',function(err){
+            if(err)console.log('already exist dir'); 
+            writeStream = fs.createWriteStream(__dirname+'/../save/post/'+part.name);
+            writeStream.filename = part.name;
+            part.pipe(writeStream);
+        });
+        
+        part.on('end',function(){
+            console.log(' Part read complete');
+            writeStream.end();
+        });
+    });
+    form.on('close',function(){
+        console.log('close');
+        var query='update pjh1352.post set content = ? where _id=?'
+        var params=[content,post_id];
+        
+        connection.query(query,params,function(err,result){
+            if(err)throw err;
+
+            var deleteQuery="";
+            for(i=0;i<deleteList.length;i++){
+                deleteQuery+='delete from pjh1352.postImage where _id ='+deleteList[i]+';';
+            }
+            var selectQuery='select * from pjh1352.post where _id = ?'
+            var selectParams=[post_id];
+            var insertQuery="";
+            for(i=0;i<files.length;i++){
+                insertQuery+="insert into pjh1352.postImage(post_id,filename) values("+post_id+",'"+files[i]+"');";
+            }
+            console.log("post id : "+post_id);
+            if(deleteQuery==""){
+                console.log("deleteQuery empty");
+                connection.query(insertQuery,function(err,rows,fields){
+                    if(err)throw err;
+                     connection.query(selectQuery,selectParams,function(err,rows,fields){
+                         if(err)throw err;
+                         res.status(200).json(new Object(rows[0]));
+                    });
+                 });
+            }else{
+                console.log("deleteQuery exist");
+                connection.query(deleteQuery,function(err,rows,fields){
+                    if(err)throw err;
+                    if(files.length!=0){
+                        connection.query(insertQuery,function(err,rows,fields){
+                            if(err)throw err;
+                            connection.query(selectQuery,selectParams,function(err,rows,fields){
+                                if(err)throw err;
+                                res.status(200).json(new Object(rows[0]));
+                            });
+                        });
+                    }else{
+                        connection.query(selectQuery,selectParams,function(err,rows,fields){
+                            if(err)throw err;
+                            res.status(200).json(new Object(rows[0]));
+                        });
+                    }
+                    
+                });
+            }
+            
+        });
+        
+    });
+    
+});
+
+
 //post에 첨부된 이미지중 1개가져오기
 router.get('/image/thumbnail/:post_id',function(req,res,next){
     var post_id=req.params.post_id;
     var query='select * from pjh1352.postImage where post_id= ? limit 1';
     var params=[post_id];
+    connection.query(query,params,function(err,rows,fields){
+        if(err)throw err;
+        if(rows.length==0){
+            console.log("사진 없어서 못보네");
+
+        }
+        if(rows.length!=0){
+            fs.stat(__dirname+'/../save/post/'+rows[0].filename,function(err){
+                if(err){
+                    throw err;
+                }else{
+                    console.log('사진 선택');
+                    var readStream=fs.createReadStream(__dirname+'/../save/post/'+rows[0].filename)
+                    readStream.pipe(res);
+                }
+            })
+        }
+    });
+});
+//postimage id 를 이용해 사진 가져오기
+router.get('/image/thumbnail/:post_id/:image_id',function(req,res,next){
+    var post_id=req.params.post_id;
+    var image_id=req.params.image_id;
+    var query='select * from pjh1352.postImage where post_id= ? and _id = ?';
+    var params=[post_id,image_id];
     connection.query(query,params,function(err,rows,fields){
         if(err)throw err;
         
@@ -83,6 +207,21 @@ router.get('/image/thumbnail/:post_id',function(req,res,next){
                 }
             })
         }
+    });
+});
+//post각각에 있는 이미지 정보
+router.get('/images/:post_id',function(req,res,next){
+    var post_id=req.params.post_id*1;
+    var query='select * from pjh1352.postImage where post_id=?';
+    var params=[post_id];
+    connection.query(query,params,function(err,rows,fields){
+        if(err)throw err;
+        var jsonObject=new Array();
+        for(i=0;i<rows.length;i++){
+            jsonObject.push(new Object(rows[i]));
+        }
+        console.log(jsonObject);
+        res.status(200).json(jsonObject);
     });
 });
 
@@ -104,6 +243,7 @@ router.get('/:group_id/:count/:offset',function(req,res,next){
         res.status(200).json(jsonObject);
     });
 });
+
 router.delete('/delete/:post_id',function(req,res,next){
     console.log("post 삭제");
     var post_id=req.params.post_id*1;
